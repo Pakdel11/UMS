ebb2788b614e2ef37675182190e7ab154ab94d3c
Habib <Habib.pakdel1121@gmail.com>
2021-04-27 15:02:05 +0200

    security database key

--- database/config/key.js
+++ database/config/key.js
@@ -1,7 +1,6 @@
 
-// Mongo pass
-// const PASS = 'test123123';
-Mongokey = 'mongodb+srv://me:test123123@test.rshzp.mongodb.net/test?retryWrites=true&w=majority'
+
+Mongokey = 'mongodb+srv://me:********@test.rshzp.mongodb.net/test?retryWrites=true&w=majority'
 
 module.exports = {
     DBP: Mongokey
